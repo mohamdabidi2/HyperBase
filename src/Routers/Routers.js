cf60f95b1a4affad914009849199f9be7f66a97e
mohamdabidi2 <mohamedabidi58@gmail.com>
2021-02-25 14:59:25 -0800

    v2

--- src/Routers/Routers.js
+++ src/Routers/Routers.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import Home from '../components/Home/Home';
 
 
 const Routers = () => {
@@ -7,7 +8,7 @@ const Routers = () => {
         <div>
             <Router>
 
-
+<Route exact path='/' component={Home}/>
             </Router>
         </div>
      );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+const Home = () => {
+    return ( 
+        <div>
+           
+            <main>
+<section id="intro1" >
+    <div id="intro"className="animate__animated animate__fadeInRight">
+        
+<div className="itemintro animate__animated animate__fadeInLeft">
+<h2 className="soustitle">Latest Design</h2>
+    <h1 className="titleintt ">Tunisian Hoods Collection</h1>
+    <div className="line"></div>
+    <p className="dtitle">Best Deal Online Shopping Store</p>
+    <Button color='black'>SHOP NOW</Button>
+</div>
+    </div>
+</section>
+            </main>
+        </div>
+     );
+}
+ 
+export default Home;
